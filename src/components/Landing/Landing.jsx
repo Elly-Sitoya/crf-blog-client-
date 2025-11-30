6b59045c8f0cf853abc2125a59ba8a6b7463061f
Elly-Sitoya <sitoyaelly@gmail.com>
2025-11-30 15:34:54 +0300

    Add landing page and restructure routing - Move blog homepage to /blogs route - Create new landing page at / route with Blog Page link - Update navigation references to point to /blogs - Fix redirect issue in Home component

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Landing.css'
+import logo from '../../assets/travelbrain-logo.svg'
+
+function Landing() {
+  return (
+    <div className="landing-page">
+      <div className="landing-container">
+        <div className="landing-content">
+          <div className="landing-logo">
+            <img src={logo} alt="Blog Logo" />
+          </div>
+          <h1 className="landing-title">Welcome to Our Blog</h1>
+          <p className="landing-description">
+            Discover amazing stories, insights, and ideas from our community of writers.
+          </p>
+          <Link to="/blogs" className="blog-page-link">
+            Blog Page
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
+
